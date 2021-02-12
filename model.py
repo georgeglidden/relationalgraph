rewrote pair sampler, replaced erroneous softmax activation w/ sigmoid, organized training methods

model.py
@@ -39,11 +39,17 @@ class MiniBatch():
 # augmentor :   augment fn
 # ds, N, M, K -> [MiniBatch(ds[i:min(i+M,N)], M, apply_random_augmentations, K) for
 #                   i in range(0,N,M)]
-def batch(dx, dy, N, M, K, augmentor = lambda x: x, shuffle=True):
+def batch(X, Y, N, M, K, augmentor = lambda x: x, shuffle=True):
+    if shuffle:
+        train_perm = tf.random.shuffle(tf.range(tf.shape(X)[0]))
+        X = tf.gather(X, train_perm, axis=0)
+        Y = tf.gather(Y, train_perm, axis=0)
     batched_ds = list()
     for i in range(0, N, M):
-        j = min(i+M, N)
-        minibatch = MiniBatch(dx[i:j], dy[i:j], M, K, augmentor)
+        j = i+M
+        if j >= N:
+            break
+        minibatch = MiniBatch(X[i:j], Y[i:j], M, K, augmentor)
         batched_ds.append(minibatch)
     if shuffle:
         np.random.shuffle(batched_ds)
@@ -94,20 +100,76 @@ def aggregate(X, Y, M, K):
     agg_t = tf.concat((pos_t,neg_t),0)
     return agg_s, agg_t
 
-class Conv4(tf.keras.Model):
-    def __init__(self):
-        super(Conv4, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(8, 3, name='bb_conv1')
+class ConvLong(tf.keras.Model):
+    def __init__(self, k=1):
+        super(ConvLong, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(8*k, 5, name='bb_conv1')
         self.norm1 = tf.keras.layers.BatchNormalization()
         self.relu1 = tf.keras.layers.ReLU()
         self.pool1 = tf.keras.layers.AvgPool2D(strides=2)
 
-        self.conv2 = tf.keras.layers.Conv2D(16, 3, name='bb_conv2')
+        self.conv2 = tf.keras.layers.Conv2D(8*k, 3, name='bb_conv2')
         self.norm2 = tf.keras.layers.BatchNormalization()
         self.relu2 = tf.keras.layers.ReLU()
         self.pool2 = tf.keras.layers.AvgPool2D(strides=2)
 
-        self.conv3 = tf.keras.layers.Conv2D(32, 3, name='bb_conv3')
+        self.conv2 = tf.keras.layers.Conv2D(16*k, 3, name='bb_conv3')
+        self.norm2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.ReLU()
+        self.pool2 = tf.keras.layers.AvgPool2D(strides=2)
+
+        self.conv2 = tf.keras.layers.Conv2D(32*k, 3, name='bb_conv4')
+        self.norm2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.ReLU()
+        self.pool2 = tf.keras.layers.AvgPool2D(strides=2)
+
+        self.conv3 = tf.keras.layers.Conv2D(64*k, 3, name='bb_conv5')
+        self.norm3 = tf.keras.layers.BatchNormalization()
+        self.relu3 = tf.keras.layers.ReLU()
+        self.pool3 = tf.keras.layers.AvgPool2D(strides=2)
+
+        self.flatten4 = tf.keras.layers.Flatten()
+        self.dense4 = tf.keras.layers.Dense(64, name='bb_dense4')
+        self.norm4 = tf.keras.layers.BatchNormalization()
+        self.relu4 = tf.keras.layers.ReLU()
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        x = self.flatten4(x)
+        x = self.dense4(x)
+        x = self.norm4(x)
+        x = self.relu4(x)
+
+        return x
+
+class Conv4(tf.keras.Model):
+    def __init__(self, k=1):
+        super(Conv4, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(8*k, 3, name='bb_conv1')
+        self.norm1 = tf.keras.layers.BatchNormalization()
+        self.relu1 = tf.keras.layers.ReLU()
+        self.pool1 = tf.keras.layers.AvgPool2D(strides=2)
+
+        self.conv2 = tf.keras.layers.Conv2D(16*k, 3, name='bb_conv2')
+        self.norm2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.ReLU()
+        self.pool2 = tf.keras.layers.AvgPool2D(strides=2)
+
+        self.conv3 = tf.keras.layers.Conv2D(32*k, 3, name='bb_conv3')
         self.norm3 = tf.keras.layers.BatchNormalization()
         self.relu3 = tf.keras.layers.ReLU()
         self.pool3 = tf.keras.layers.AvgPool2D(strides=2)
@@ -143,10 +205,10 @@ class Conv4(tf.keras.Model):
 class Relator(tf.keras.Model):
     def __init__(self):
         super(Relator, self).__init__()
-        self.d1 = tf.keras.layers.Dense(256, name='rel_dense1')
+        self.d1 = tf.keras.layers.Dense(32, name='rel_dense1')
         self.bn1 = tf.keras.layers.BatchNormalization()
         self.lru = tf.keras.layers.LeakyReLU()
-        self.d2 = tf.keras.layers.Dense(1,activation='softmax', name='rel_dense2')
+        self.d2 = tf.keras.layers.Dense(1,activation='sigmoid', name='rel_dense2')
 
     def call(self, x):
         x = self.d1(x)
@@ -159,7 +221,8 @@ class Relator(tf.keras.Model):
 def loss(T, Y):
     return loss_object(T,Y)
 
-def train_step(X, T1, M, K, step):
+def train_step(model, X, T1, M, K, step):
+    backbone, relation = model
     with tf.GradientTape(persistent=True) as tape:
         assert not np.any(np.isnan(X))
         assert not np.any(np.isnan(T1))
@@ -179,16 +242,14 @@ def train_step(X, T1, M, K, step):
     optimizer.apply_gradients(zip(relation_gradients, relation.trainable_variables))
     backbone_gradients = tape.gradient(model_loss, backbone.trainable_variables)
     optimizer.apply_gradients(zip(backbone_gradients, backbone.trainable_variables))
-    train_loss(T2, Y)
-    train_accuracy(T2, Y)
+    return T2, Y
 
-def test_step(X, T1, M, K):
+def test_step(model, X, T1, M, K):
+    backbone, relation = model
     Z1 = backbone(X, training=False)
     Z2, T2 = aggregate(Z1, T1, M, K)
     Y = relation(Z2, training=False)
-    l = loss(T2, Y)
-    test_loss(T2, Y)
-    test_accuracy(T2, Y)
+    return T2, Y
 
 if not __name__ == '__main__':
     print('model.py')
@@ -245,7 +306,7 @@ else:
         for i in range(N1):
             minibatch = train_batches[i]
             train_batch_x, train_batch_y = minibatch()
-            train_step(train_batch_x, train_batch_y, M, K, i%2)
+            train_step([backbone, relation], train_batch_x, train_batch_y, M, K, i%2)
             training_time_sum = time() - training_time_start
             if i % (N1//p) == 0:
                 print(
@@ -257,7 +318,7 @@ else:
         for i in range(N2):
             minibatch = test_batches[i]
             test_batch_x, test_batch_y = minibatch()
-            test_step(test_batch_x, test_batch_y, M, K)
+            test_step([backbone, relation], test_batch_x, test_batch_y, M, K)
             if i % (N2//p) == 0:
                 print(
                 f'{100*i/N2}%')

train_job.py
@@ -1,11 +1,12 @@
 from model import Conv4, ConvLong, Relator, batch, train_step, test_step, aggregate
+from generate_pairs import pair
 from augmentations import apply_all, no_aug
 import tensorflow as tf
 from tensorflow.keras import datasets as keras_datasets
 import sys
 import smtplib, ssl
 
-DO_MAIL = False
+DO_MAIL = True
 
 def load_ds(ds_name):
     print('loading', ds_name)
@@ -94,8 +95,7 @@ if DO_MAIL:
     with smtplib.SMTP_SSL("smtp.gmail.com") as server:
         server.login(u, p)
         print('successful login to ', u)
-        server.sendmail(u,r,
-            f"""\
+        server.sendmail(u,r, f"""\
 Subject: train job {id} START
 
 id: {id}
@@ -104,24 +104,33 @@ encoder:\n{model_a_summary}
 relation:\n{model_b_summary}""")
 
 # do training
-p = 100
+nsteps = N1 // M #approximately
+p = M * (nsteps // 1000) # update every 0.1%
+s = M * (nsteps // 20) # save every 5%
+test_nsteps = N2 // M
+p2 = M * (nsteps // 1000)
+stats_list = []
 train_accuracy = 0.0
 train_accuracy_metric = tf.keras.metrics.BinaryAccuracy()
+train_loss_metric = tf.keras.metrics.BinaryCrossentropy()
 test_accuracy = 0.0
 test_accuracy_metric = tf.keras.metrics.BinaryAccuracy()
 for epoch in range(int(state['epochs'])):
+    stats_list = []
     train_accuracy_metric.reset_states()
+    train_loss_metric.reset_states()
     test_accuracy_metric.reset_states()
     train_batches = batch(train_x, train_y, N1, M, K, augmentor=augment_opt)
     test_batches = batch(test_x, test_y, N2, M, K, augmentor=augment_opt)
 
     print('training')
-    for i in range(N1):
-        minibatch = train_batches[i]
+    i = 0
+    for minibatch in train_batches:
+        i += M
         train_batch_x, train_batch_y = minibatch()
         with tf.GradientTape() as tape:
             Z1 = model_a(train_batch_x, training=True)
-            Z2, T2 = aggregate(Z1, train_batch_y, M, K)
+            Z2, T2 = pair(Z1, M, K)
             Y = model_b(Z2, training=True)
             loss_value = loss(T2, Y)
         grads_a, grads_b = tape.gradient(loss_value, [model_a.trainable_weights, model_b.trainable_weights])
@@ -129,21 +138,29 @@ for epoch in range(int(state['epochs'])):
         optimizer.apply_gradients(zip(grads_b, model_b.trainable_weights))
 
         train_accuracy_metric.update_state(T2, Y)
+        train_loss_metric.update_state(T2,Y)
+        if i % s == 0:
+            print('network savepoint')
+            model_a.save(f'model_tests/id{id}_encoder_epoch{epoch}_step{i}_loss{str(train_loss_metric.result())[:5]}')
+            model_b.save(f'model_tests/id{id}_relator_epoch{epoch}_step{i}_loss{str(train_loss_metric.result())[:5]}')
         if i % p == 0:
             print(
             f'{100*i/N1}% '
             f'loss {loss_value} '
-            f'acc {train_accuracy_metric.result()} ')
+            f'acc {train_accuracy_metric.result()} '
+            f'avg loss {train_loss_metric.result()}\n'
+            f'{tf.reshape(Y-T2, [-1])}')
 
     print('testing')
-    for i in range(N2):
-        minibatch = test_batches[i]
+    i = 0
+    for minibatch in test_batches:
+        i += M
         test_batch_x, test_batch_y = minibatch()
         Z1 = model_a(test_batch_x, training=True)
         Z2, T2 = aggregate(Z1, test_batch_y, M, K)
         Y = model_b(Z2, training=True)
         test_accuracy_metric.update_state(T2,Y)
-        if i % p == 0:
+        if i % p2 == 0:
             print(
             f'{100*i/N2}%')
     train_accuracy = train_accuracy_metric.result()
@@ -153,22 +170,21 @@ for epoch in range(int(state['epochs'])):
     if DO_MAIL:
         with smtplib.SMTP_SSL("smtp.gmail.com") as server:
             server.login(u, p)
-            server.sendmail(u,r,
-                f"""\
+            server.sendmail(u,r, f"""\
 Subject: train job {id} EPOCH {epoch}
 
 id: {id}
 state:\n{state}
 train accuracy: {train_accuracy}
-test accuracy: {test_accuracy}""")
+test accuracy: {test_accuracy}
+{stats_list}""")
 
 
 # update me when complete\
 if DO_MAIL:
     with smtplib.SMTP_SSL("smtp.gmail.com") as server:
         server.login(u, p)
-        server.sendmail(u,r,
-            f"""\
+        server.sendmail(u,r, f"""\
 Subject: train job {id} STOP
 
 id: {id}